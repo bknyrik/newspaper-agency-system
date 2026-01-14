7b6c2292227e15843955f738a7e86827911f22a6
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-14 13:09:28 +0200

    Add model Newspaper

--- editor/models.py
+++ editor/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Topic(models.Model):
     name = models.CharField(max_length=64, unique=True)
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_data = models.DateField()
+    topics = models.ManyToManyField(Topic, related_name="newspapers")
