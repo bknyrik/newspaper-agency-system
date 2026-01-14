bdb932d2ad4a86ecbcf2d9d01c0a1975ae4e2e61
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-14 18:02:34 +0200

    Add test for method __str__ of the Topic

--- editor/tests/tests_models.py
+++ editor/tests/tests_models.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from editor.models import Topic, Newspaper, Redactor
+
+
+class ModelsTests(TestCase):
+
+    def test_topic_str(self) -> None:
+        name = "IT"
+        topic = Topic.objects.create(name=name)
+        self.assertEqual(str(topic), name)
