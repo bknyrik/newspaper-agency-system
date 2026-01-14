b813b9028d8b40d8ce0a687ed1df01c2df9541d2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-14 18:30:17 +0200

    Add test for method __str__ of the Newspaper

--- editor/tests/tests_models.py
+++ editor/tests/tests_models.py
@@ -9,3 +9,20 @@ class ModelsTests(TestCase):
         name = "IT"
         topic = Topic.objects.create(name=name)
         self.assertEqual(str(topic), name)
+
+
+    def test_newspaper_str(self) -> None:
+        topics = Topic.objects.bulk_create(
+            (Topic(name="IT"), Topic(name="Politics"))
+        )
+        newspaper = Newspaper.objects.create(title="About Django")
+        newspaper.topics.set(topics)
+
+        self.assertEqual(
+            str(newspaper),
+            f"{newspaper.title} "
+            f"(date: {newspaper.published_date} "
+            f"topics: {", ".join(
+                str(topic) for topic in newspaper.topics.all()
+            )})"
+        )
