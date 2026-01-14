eb0b7d0574b1bbc3cf78fcccaa31504d147cf49a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-14 17:16:55 +0200

    Add class Meta with option ordering to the Topic

--- editor/models.py
+++ editor/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth import settings
 class Topic(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    class Meta:
+        ordering = ("name", )
+
     def __str__(self) -> str:
         return self.name
 
