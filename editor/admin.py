1fa58064efb72add05e89c941ffefc4da79ce274
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-15 21:10:58 +0200

    Register Newspaper to admin panel with options list_display, list_filter and search_fields

--- editor/admin.py
+++ editor/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from editor.models import Topic
+from editor.models import Topic, Newspaper
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "published_date", "get_topics")
+    list_filter = (
+        "title",
+        "published_date",
+        "topics",
+        "publishers__username"
+    )
+    search_fields = ("title", )
+
+    def get_topics(self, newspaper: Newspaper) -> str:
+        return ", ".join(str(topic) for topic in newspaper.topics.all())
 
 
 admin.site.register(Topic)
