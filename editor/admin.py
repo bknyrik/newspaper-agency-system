9fe5b8950393b36833cf9dd6879ce58f69844406
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-16 13:36:44 +0200

    Add custom filter by years_of_experience to RedactorAdmin

--- editor/admin.py
+++ editor/admin.py
@@ -54,10 +54,48 @@ class PublishersListFilter(admin.SimpleListFilter):
         return queryset.all()
 
 
+class YearsOfExperienceListFilter(admin.SimpleListFilter):
+    title = "years of experience"
+    parameter_name = "years_of_experience"
+
+    def lookups(
+        self,
+        request: HttpRequest,
+        model_admin: "RedactorAdmin"
+    ) -> tuple[tuple[str, str], ...]:
+        return (
+            ("0,2", "0-2"),
+            ("2,5", "2-5"),
+            ("5,10", "5-10"),
+            ("10,15", "10-15"),
+            ("15,20", "15-20"),
+            ("20", "20+")
+        )
+
+    def queryset(
+        self,
+        request: HttpRequest,
+        queryset: QuerySet[Redactor]
+    ) -> QuerySet[Redactor]:
+        if self.value():
+            if self.value() == "20":
+                queryset = queryset.filter(
+                    years_of_experience__gte=20
+                )
+            else:
+                queryset = queryset.filter(
+                    years_of_experience__range=tuple(
+                        int(years) for years in self.value().split(",")
+                    )
+                )
+
+        return queryset
+
+
 @admin.register(Redactor)
 class RedactorAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("years_of_experience",)
-    list_filter = UserAdmin.list_filter + ("years_of_experience", )
+    list_filter = UserAdmin.list_filter + (YearsOfExperienceListFilter, )
     fieldsets = UserAdmin.fieldsets + (
         (
             "Additional info",
