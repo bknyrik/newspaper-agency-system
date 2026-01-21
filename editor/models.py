2b5acf5d6ec7b89406385dea0faef88d10a928e0
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-21 22:37:16 +0200

    Add model Redactor

--- editor/models.py
+++ editor/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
 
 
 class Topic(models.Model):
@@ -9,3 +11,13 @@ class Topic(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(
+        null=True,
+        blank=True,
+        validators=(
+            validators.MinValueValidator(limit_value=1),
+        )
+    )
