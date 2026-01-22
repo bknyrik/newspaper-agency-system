7498d55a8ea3f19cac4e4190a48bf579552a1854
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-22 12:03:55 +0200

    Remove model Redactor from import in test_models.py

--- editor/tests/test_models.py
+++ editor/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from editor.models import Topic, Newspaper, Redactor
+from editor.models import Topic, Newspaper
 
 
 class ModelTests(TestCase):
