778b2d8a2d6c1ce7f8805bc80dafe659674393f8
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-22 13:03:02 +0200

    Add RedactorAdmin and register Redactor to admin panel

--- editor/admin.py
+++ editor/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from editor.models import Topic
+from editor.models import Topic, Redactor
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    empty_value_display = "Absent"
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = (
+        UserAdmin.fieldsets +
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = (
+        UserAdmin.add_fieldsets +
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "email",
+                        "years_of_experience",
+                    )
+                }
+            ),
+        )
+    )
 
 
 admin.site.register(Topic)
