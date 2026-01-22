e71b8e286fc5559c410d820cf2fcfa86892405b4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-22 14:05:24 +0200

    Add custom filter YearsOfExperienceListFilter to RedactorAdmin

--- editor/admin.py
+++ editor/admin.py
@@ -1,13 +1,57 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.http import HttpRequest
+from django.db.models import QuerySet
 
 from editor.models import Topic, Redactor
+
+
+class YearsOfExperienceListFilter(admin.SimpleListFilter):
+    title = "years of experience"
+    parameter_name = "years_of_experience"
+
+    def lookups(
+        self,
+        request: HttpRequest,
+        model_admin: "RedactorAdmin"
+    ) -> tuple[tuple[str, str], ...]:
+        return (
+            ("0", "Absent"),
+            ("1,3", "1-3"),
+            ("3,5", "3-5"),
+            ("5,10", "5-10"),
+            ("10,15", "10-15"),
+            ("15,20", "15-20"),
+            ("20", "20+"),
+        )
+
+    def queryset(
+        self,
+        request: HttpRequest,
+        queryset: QuerySet[Redactor]
+    ) -> QuerySet[Redactor]:
+        if self.value():
+            expression = {}
+            match self.value():
+                case "0":
+                    expression["years_of_experience__isnull"] = True
+                case "20":
+                    expression["years_of_experience__gte"] = 20
+                case _:
+                    expression["years_of_experience__range"] = tuple(
+                        int(year) for year in self.value().split(",")
+                    )
+
+            queryset = queryset.filter(**expression)
+
+        return queryset
 
 
 @admin.register(Redactor)
 class RedactorAdmin(UserAdmin):
     empty_value_display = "Absent"
     list_display = UserAdmin.list_display + ("years_of_experience",)
+    list_filter = UserAdmin.list_filter + (YearsOfExperienceListFilter,)
     fieldsets = (
         UserAdmin.fieldsets +
         (("Additional info", {"fields": ("years_of_experience",)}),)
