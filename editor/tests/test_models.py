099c428e194f9c1c5fa856fa6c5921a7772adc42
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-22 11:20:22 +0200

    Add test to method __str__ of model Newspaper

--- editor/tests/test_models.py
+++ editor/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from editor.models import Topic
+from editor.models import Topic, Newspaper
 
 
 class ModelTests(TestCase):
@@ -9,3 +9,21 @@ class ModelTests(TestCase):
         name = "Test topic"
         topic = Topic.objects.create(name=name)
         self.assertEqual(str(topic), name)
+
+    def test_newspaper_str(self) -> None:
+        topics = Topic.objects.bulk_create(
+            (
+                Topic(name="Test topic"),
+                Topic(name="Test topic 2")
+            )
+        )
+        newspaper = Newspaper.objects.create(
+            title="Test title"
+        )
+        newspaper.topics.set(topics)
+        self.assertEqual(
+            str(newspaper),
+            f"{newspaper.title} "
+            f"(date: {newspaper.published_date} "
+            f"topics: {topics[0]}, {topics[1]})"
+        )
