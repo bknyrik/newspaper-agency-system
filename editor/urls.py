6880ba16c93324f3d6f4a8ac288b084f877fbb9c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-23 11:35:38 +0200

    Add path with TopicUpdateView to urlpatterns of editor urls.py

--- editor/urls.py
+++ editor/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from editor.views import (
     IndexView,
     TopicListView,
-    TopicCreateView
+    TopicCreateView,
+    TopicUpdateView,
 )
 
 
@@ -11,6 +12,11 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
     path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update"
+    ),
 ]
 
 app_name = "editor"
