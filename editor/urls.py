092c0b5fd36ae393b78a0b7e3eeab400d40c7e8c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-23 10:42:20 +0200

    Add path with TopicCreateView to urlpatterns of editor urls.py

--- editor/urls.py
+++ editor/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from editor.views import (
     IndexView,
     TopicListView,
+    TopicCreateView
 )
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
 ]
 
 app_name = "editor"
