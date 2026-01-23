dd37bb74906ad70979ebd6d671718385ca4ae440
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-23 09:35:50 +0200

    Add path with TopicListView to urlpatterns of editor urls.py

--- editor/urls.py
+++ editor/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from editor.views import (
     IndexView,
+    TopicListView,
 )
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
 ]
 
 app_name = "editor"
