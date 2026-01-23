498d13f0677e24d5dccf28d1830e3f30e839bd6f
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-23 09:17:10 +0200

    Add view IndexView with method get

--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,16 @@
+from django.views import generic
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from editor.models import Topic, Newspaper, Redactor
+
+
+class IndexView(generic.View):
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            "count_topics": Topic.objects.count(),
+            "count_newspapers": Newspaper.objects.count(),
+            "count_redactors": Redactor.objects.count()
+        }
+        return render(request, "editor/index.html", context)
