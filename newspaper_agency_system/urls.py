ab7fd756abf171db83a1071f42f27d44eb83ef09
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-24 12:37:43 +0200

    Add path with debug_toolbar.urls to urlpatterns of project urls.py

--- newspaper_agency_system/urls.py
+++ newspaper_agency_system/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("editor.urls", namespace="editor"))
+    path("", include("editor.urls", namespace="editor")),
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
