a9a015de523dfd6f02c01dfebbc12a70bfb25e81
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-02-27 16:37:15 +0200

    Add test case PrivateTopicTests with overriden method setUp

--- editor/tests/test_views.py
+++ editor/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 
 
@@ -10,3 +11,13 @@ class PublicTopicTests(TestCase):
     def test_login_required(self) -> None:
         response = self.client.get(TOPIC_URL)
         self.assertNotEqual(response.status_code, 200)
+
+
+class PrivateTopicTests(TestCase):
+
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="test12345"
+        )
+        self.client.force_login(self.user)
