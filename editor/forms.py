f124ffdd1c1108b99af6de123f5115ff773c1bc3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-03-13 15:38:20 +0200

    Add custom form RedactorCreationForm

--- editor/forms.py
+++ editor/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-from editor.models import Topic, Newspaper
+from editor.models import Topic, Newspaper, Redactor
 
 
 class TopicForm(forms.ModelForm):
@@ -24,3 +25,34 @@ class TopicForm(forms.ModelForm):
         self.save_m2m()
         topic.newspapers.set(self.cleaned_data["newspapers"])
         return topic
+
+
+class RedactorCreationForm(UserCreationForm):
+
+    years_of_experience = forms.IntegerField(min_value=1, required=False)
+    newspapers = forms.ModelMultipleChoiceField(
+        queryset=Newspaper.objects.prefetch_related("topics"),
+        widget=forms.SelectMultiple,
+        required=False
+    )
+
+    class Meta(UserCreationForm.Meta):
+
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "years_of_experience",
+            "newspapers"
+        )
+
+    def save(self, commit: bool = ...) -> Redactor:
+        redactor = super().save(commit=False)
+
+        if commit:
+            redactor.save()
+
+        self.save_m2m()
+        redactor.newspapers.set(self.cleaned_data["newspapers"])
+        return redactor
