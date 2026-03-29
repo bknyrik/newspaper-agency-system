0fd5dafe4ed96f91b4c8577c4700ea442c8b9fb6
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-03-29 21:56:32 +0300

    Remove key class in attrs of widget of name in TopicSearchForm

--- editor/forms.py
+++ editor/forms.py
@@ -38,9 +38,7 @@ class TopicSearchForm(forms.Form):
         max_length=255,
         required=False,
         label="",
-        widget=forms.TextInput(attrs={
-            "class": "form-control",
-            "placeholder": "Search by name"}
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}
         )
     )
 
