ef80e0871588801e65748d66252ea56b8841295e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-27 01:01:37 +0300

    Update project settings.py

--- newspaper_agency_system/settings.py
+++ newspaper_agency_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#dce&i!ojoq)(#$7g)le$@t!-^w3qcf23rtas=10%gaj53n(u4"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+ "django-insecure-#dce&i!ojoq)(#$7g)le$@t!-^w3qcf23rtas=10%gaj53n(u4"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
